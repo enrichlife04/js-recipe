c68323fdabe7ff58a6d83556a0a2c4b726cf7440
enrichlife04 <enrichlife0429@gmail.com>
2021-07-02 17:48:36 +0900

    カウンターを作成した

---
+++ 02_レシピ/01_3の倍数/main.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line no-unused-vars
+const number = 24
+
+// Step 1
+
+// if (number % 3 === 0) {
+//   console.log(number + "ああああああああ")
+// } else {
+//   console.log(number)
+// }
+
+// Step 2
+// for (let n = 1; n <= number; n++) {
+//   if (n % 3 === 0) {
+//     console.log(n + "!!!!!!!!!!!!!!")
+//   } else {
+//     console.log(n)
+//   }
+// }
+
+// Step 3
+// eslint-disable-next-line no-unused-vars
+const genkiFunction = function(number) {
+  // n を 1 から、 number まで、 値を 1 ずつ増やしながら、 for の {} の中身を繰り返す
+  for (let n = 1; n <= number; n++) {
+    if (n % 3 === 0) {
+      console.log(`${n}!!!!!!!`)
+    } else {
+      console.log(n)
+    }
+  }
+}
