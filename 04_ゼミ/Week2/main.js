0128156bdfc6020f4349e3b6ce293a4825dce175
enrichlife04 <enrichlife0429@gmail.com>
2021-07-18 19:41:26 +0900

    自販機作った！！

--- 04_ゼミ/Week2/main.js
+++ 04_ゼミ/Week2/main.js
@@ -2,14 +2,27 @@ const memoInput = document.getElementById("memo-input")
 const addButton = document.getElementById("add-button")
 const memoContainer = document.getElementById("memo-container")
 
+let memos = []
+
 addButton.onclick = function() {
   console.log(memoInput.value)
+  // 1.入力内容を取得
   const text = memoInput.value
 
-  const div = document.createElement("div")
-  div.textContent = text
+  // 2. 配列memosに保存
+  memos.push(text)
+  console.log(memos)
 
-  memoContainer.append(div)
+  // 3. 配列memosの内容を全部表示
+  memoContainer.innerHTML = ""
+
+  for (let i = 0; i < memos.length; i++) {
+    //3-1 divを作って
+    const div = document.createElement("div")
+    div.textContent = memos[i]
+
+    memoContainer.append(div)
+  }
 
   memoInput.value = ""
 }
